1cccfafa46c6dc87045fb97f53d44755a8ce9114
ThurFerreira <arthurfsilva110@gmail.com>
2022-11-09 18:38:39 -0300

    teste

--- Ex1/src/Juros.java
+++
@@ -1,8 +0,0 @@
-public class Juros {
-    public static void CalculaJuros(int valor){
-        double valorFinal = valor + (0.8 * valor);
-
-        //System.out.println("Com esse valor, voce terá " + valorFinal + " por mes");
-        System.out.format("%.2f\n", valorFinal);
-    }
-}
